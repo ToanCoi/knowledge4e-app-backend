42f7bb0801b30181aca0dc90630913865dde92fc
haidv <haidv2801@gmail.com>
2023-10-31 23:33:38 +0700

    CRUD group

---
+++ Knowledge4e/Knowledge4e.Entities/Entities/Group/Group.cs
@@ -0,0 +1,19 @@
+﻿using Knowledge4e.Core.Entities;
+using Knowledge4e.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge4e.Entities.Entities.Group
+{
+    [ConfigTables(tableName: "room", HasDeletedColumn = true, UniqueColumns = "RoomName")]
+    public class KGroup : BaseEntity
+    {
+        public string TopicID { get; set; }
+        public string RoomName { get; set; }
+        public int MemberLimited { get; set; }
+        public int CreaterID { get; set; }
+    }
+}
---
+++ Knowledge4e/Knowledge4e.Infarstructure/Repositories/GroupRepository/IGroupRepository.cs
@@ -0,0 +1,11 @@
+﻿using Knowledge4e.Core.Entities.Account;
+using Knowledge4e.Entities.Entities.Group;
+using Knowledge4e.Infarstructure.Repositories.BaseRepository;
+
+namespace Knowledge4e.Infarstructure.Repositories
+{
+    public interface IGroupRepository : IBaseRepository<KGroup>
+    {
+
+    }
+}
